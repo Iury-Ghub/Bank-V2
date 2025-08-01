765d7625cf12bc57c67689367c3ed7672a2cb287
Iury-Ghub <joseiury74@gmail.com>
2025-08-01 19:57:29 -0300

    first commit: Iury's Bank V1 add to the project

---
+++ Bank.py
@@ -0,0 +1,57 @@
+bank=('''
+[D] Deposit
+[W] withdrawal
+[E] Extract
+[X] Exit
+
+To select any option => ''')
+
+balance=0
+limit_withdrawal=500.00
+extract=""
+withdrawal_number=0
+WITHDRAWAL_LIMIT=3
+option=0
+result_num=result_txt=0
+while True:
+    title("WELCOME TO THE IURY'S BANK", 50)
+    balance_status(balance)
+    try:
+        option=input(bank).strip().upper()[0]
+    except:
+        option=0
+    if option=="D":
+        title("DEPOSIT AREA", 50)
+        balance_status(balance)
+        p = quest(f"Would you like to deposit")
+        if p == "Y":
+            result_num,result_txt=deposit("Enter your deposit amount: $ ")
+            balance+=result_num
+            extract+=result_txt
+    elif option=="W":
+        title("WITHDRAWAL AREA", 50)
+        balance_status(balance)
+        print(f"Withdrawal limit is ${limit_withdrawal:.2f}, {WITHDRAWAL_LIMIT} times per day")
+        p=quest(f"Would you like to withdrawal")
+        if p == "Y":
+            if withdrawal_number <= 3:
+                result_num,result_txt =withdrawal("Enter your withdrawal amount: $ ",500.00,balance)
+                balance-=result_num
+                extract+=result_txt
+                withdrawal_number+=1
+            else:
+                print("You have exceeded your daily withdrawal limit")
+    elif option=="E":
+        title("EXTRACT", 50)
+        print(f"Your Extract is here:\n0{extract}")
+        while True:
+            p=input("press enter to go back: ")
+            if p == "":
+                break
+
+    elif option=="X":
+        title("GODBYE, SEE YOU LATER...", 50)
+        break
+    else:
+        print("Please enter a valid option")
+        sleep(1.5)
---
+++ utilities/Functions.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from time import sleep
+
+def title(tex,tam):
+    print('-'*tam)
+    print(tex.center(tam))
+    print('-'*tam)
+
+def deposit(txt="", limit=1500):
+    while True:
+        try:
+            while True:
+                d = float(input(txt))
+                if d > limit:
+                    print(f"You can only make deposits up to ${limit:.2f}")
+                    sleep(1)
+                elif d < 0.01:
+                    print(f"You can only make deposits below $ 0.01")
+                    sleep(1)
+                else:
+                    break
+        except ValueError:
+            print("Enter a valid number")
+        else:
+            now = datetime.now()
+            format_now = now.strftime("%d/%m/%Y %I:%M:%S %p")
+            dep = f"You deposited ${d:.2f} {format_now}\n"
+            print(dep)
+            sleep(1.5)
+            return d,dep
+
+def withdrawal(txt="",limit=500.00, balance=0):
+
+    while True:
+        try:
+            while True:
+                w = float(input(txt))
+                if w > balance:
+                    print("This amount exceeds your balance")
+                elif w > limit:
+                    print("You have exceeded your withdrawal limit")
+                elif w < 0.01:
+                    print("This amount is less than 0.01")
+                else:
+                    break
+        except ValueError:
+            print("Enter a valid number")
+        else:
+            now = datetime.now()
+            format_now=now.strftime("%d/%m/%Y %I:%M:%S %p")
+            a=f"You withdrew ${w:.2f} {format_now}\n"
+            print(a)
+            sleep(1.5)
+            return w,a
+
+def quest(tex="",y="Y",n="N"):
+    while True:
+        try:
+            q=input(f"{tex} ({y}/{n}): ").strip().upper()[0]
+        except:
+            q=0
+        if q == "Y":
+            return y
+        elif q == "N":
+            return n
+        else:
+            print("Enter a valid option")
+def balance_status(b):
+    print(f"Your balance : {b:.2f}".center(50))
+    print("-"*50)
+
+
+
+
+
+
